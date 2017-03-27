d8e8773df9b006c866b02015cf342fc774eb671f
Jolteon79 <ecw@wfour.net>
2017-03-27 10:43:50 -0500

    Add files via upload

---
+++ Blinking_LED.ino
@@ -0,0 +1,16 @@
+//changes for challenge: delays @ 500 to change rate, added another resistor for brightness
+
+int ledPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+}
