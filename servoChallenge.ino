30ad96acc0f4673a88720ae1c6af3c187d4e06cd
Jolteon79 <ecw@wfour.net>
2017-04-03 10:58:01 -0500

    Add files via upload

---
+++ servoChallenge.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+int potmtr = 1;
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(10);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  pos = analogRead(potmtr) * 0.175;
+  
+  myservo.write(pos);              
+  delay(15);                     
+  
+}
+
